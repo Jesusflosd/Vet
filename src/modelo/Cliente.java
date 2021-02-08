ae58d6d4095ce649f5c8aec1ccde55597ec4615f
Lince <miguelhumberto522@gmail.com>
2021-02-07 18:01:21 -0600

    mascota y cliente añadido

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 /**
@@ -10,5 +5,44 @@ package modelo;
  * @author migue
  */
 public class Cliente {
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private Mascota mascota;
+    
+    public Cliente(String nombre, String telefono, String correo, String nombreM, String edad, String peso, String raza, String id){
+        mascota = new Mascota(nombreM, edad, peso, raza, id);
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }  
     
 }
--- src/modelo/Mascota.java
+++ src/modelo/Mascota.java
@@ -5,5 +5,61 @@ package modelo;
  * @author migue
  */
 public class Mascota {
+    private String nombre;
+    private String edad;
+    private String raza;
+    private String peso;
+    private String id;
+    
+    public Mascota(String nombre, String edad, String raza, String peso, String id) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+        this.peso = peso;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    
     
 }
